5203de07cc9e32311bc2f182fb71d97a015a5b7b
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2024-11-14 11:01:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "ap-south-1"
+}  
+resource "aws_instance" "server" {
+  ami                    = "ami-0dee22c13ea7a9a67"
+  instance_type          = "t2.micro"
+ }
+tags{
+Name= "Terraformserver"
+}
