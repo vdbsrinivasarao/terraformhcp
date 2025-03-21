2acb871dbb8c065820ec1c4a501d88c4c11ce1c2
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2025-03-21 12:55:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }  
 resource "aws_instance" "server" {
   count = 2
-  ami                    = "ami-0dee22c13ea7a9a67"
+  ami                    = "ami-06b6e5225d1db5f46"
   instance_type          = "t2.micro"
  
 tags= {
