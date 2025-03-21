a4b9a5a4bc16442ec6c7b5b4e726e7fc4a083fc3
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2025-03-21 13:01:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "ap-south-1"
 }  
 resource "aws_instance" "server" {
-  count = 2
+  count = 1
   ami                    = "ami-06b6e5225d1db5f46"
   instance_type          = "t2.micro"
  
 tags= {
-Name= "Terraformserver"
+Name= "Terraformtestserver"
 }
 }
