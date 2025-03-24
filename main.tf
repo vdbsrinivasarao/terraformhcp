a8e06dc8b4d3a039a0c3935627162a1563aea84f
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2025-03-24 19:34:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ provider "aws" {
   region = "ap-south-1"
 }  
 resource "aws_instance" "server" {
-  count = 1
+  count = 4
   ami                    = "ami-06b6e5225d1db5f46"
-  instance_type          = "t2.micro"
- 
-tags= {
-Name= "Terraformtestserver"
+  instance_type          = "t2.medium"
+  key_name= "durgakeypair.pem"
+  vpc_security_group_ids = ["sg-0af871da3173f77dc"]
+  tags ={
+  Name= var.instance_names [count_index]
+  }
 }
+variable "instance_names" {
+default = ["Jenkins","Nexus","Tomcat1","Tomcat2"]
 }
