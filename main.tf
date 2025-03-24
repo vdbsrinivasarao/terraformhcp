f5b261dd0d741f3eeea709e6f8617dad26601871
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2025-03-24 19:59:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
 }  
-resource "aws_instance" "server" {
+resource "aws_instance" "ONE" {
   count = 4
   ami                    = "ami-06b6e5225d1db5f46"
   instance_type          = "t2.medium"
