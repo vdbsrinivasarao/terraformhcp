ebafad8b8d4cb729c9abd0a5f3b6a7bb1e6cd420
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2025-03-24 20:00:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ONE" {
   count = 4
   ami                    = "ami-06b6e5225d1db5f46"
   instance_type          = "t2.medium"
-  key_name= "durgakeypair.pem"
+  key_name= "durgakeypair"
   vpc_security_group_ids = ["sg-0af871da3173f77dc"]
  
 }
